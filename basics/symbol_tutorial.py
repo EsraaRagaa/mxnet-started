b47db778bd4a239c9e455f40afc2fa88c2b24baf
socurites <socurites@gmail.com>
2017-04-11 11:12:50 +0900

    update symbol_tutorial.py

--- basics/symbol_tutorial.py
+++ basics/symbol_tutorial.py
@@ -3,20 +3,17 @@ import mxnet as mx
 '''
 Symobol Composition
 '''
-# Basic Operators
+# Configure computation graph
 a = mx.sym.Variable('a')
 b = a * 2 + 1
-ex = b.bind(ctx=mx.cpu(), args={'a': mx.nd.ones((2, 3))})
-ex.forward()
-print(ex.outputs)
-print(len(ex.outputs))
-print(ex.outputs[0].asnumpy())
 
-a = mx.sym.Variable('a')
-b = mx.sym.Variable('b')
-c = a + b
-ex = c.bind(ctx=mx.cpu(), args={'a': mx.nd.ones((2, 3)),
-                                'b': mx.nd.full((2, 3), 3)})
+# Plot computation graph
+vis = mx.viz.plot_network(symbol=b)
+vis.render('graph')
+
+# Bind data
+ex = b.bind(ctx=mx.cpu(), args={'a': mx.nd.ones((2, 3))})
+# Execute computation graph
 ex.forward()
 print(ex.outputs)
 print(len(ex.outputs))
@@ -41,46 +38,17 @@ def ConvFactory(data, num_filter, kernel, stride=(1, 1), pad=(0, 0), name=None, 
 
 
 prev = mx.symbol.Variable(name="Previos Output")
-conv_comp = ConvFactory(data=prev, num_filter=64, kernel=(7, 7), stride=(2, 2))
+conv_comp1 = ConvFactory(data=prev, num_filter=64, kernel=(7, 7), stride=(2, 2), name='Conv1')
+conv_comp2 = ConvFactory(data=conv_comp1, num_filter=32, kernel=(7, 7), stride=(2, 2), name='Conv2')
 shape = {"Previos Output": (128, 3, 28, 28)}
-vis = mx.viz.plot_network(symbol=conv_comp, shape=shape)
+vis = mx.viz.plot_network(symbol=conv_comp2, shape=shape)
 vis.render('conv-factory')
 
 
-def InceptionFactoryA(data, num_1x1, num_3x3red, num_3x3, num_d3x3red, num_d3x3, pool, proj, name):
-  # 1x1
-  c1x1 = ConvFactory(data=data, num_filter=num_1x1, kernel=(1, 1), name=('%s_1x1' % name))
-  # 3x3 reduce + 3x3
-  c3x3r = ConvFactory(data=data, num_filter=num_3x3red, kernel=(1, 1), name=('%s_3x3' % name), suffix='_reduce')
-  c3x3 = ConvFactory(data=c3x3r, num_filter=num_3x3, kernel=(3, 3), pad=(1, 1), name=('%s_3x3' % name))
-  # double 3x3 reduce + double 3x3
-  cd3x3r = ConvFactory(data=data, num_filter=num_d3x3red, kernel=(1, 1), name=('%s_double_3x3' % name),
-                       suffix='_reduce')
-  cd3x3 = ConvFactory(data=cd3x3r, num_filter=num_d3x3, kernel=(3, 3), pad=(1, 1), name=('%s_double_3x3_0' % name))
-  cd3x3 = ConvFactory(data=cd3x3, num_filter=num_d3x3, kernel=(3, 3), pad=(1, 1), name=('%s_double_3x3_1' % name))
-  # pool + proj
-  pooling = mx.symbol.Pooling(data=data, kernel=(3, 3), stride=(1, 1), pad=(1, 1), pool_type=pool,
-                              name=('%s_pool_%s_pool' % (pool, name)))
-  cproj = ConvFactory(data=pooling, num_filter=proj, kernel=(1, 1), name=('%s_proj' % name))
-  # concat
-  concat = mx.symbol.Concat(*[c1x1, c3x3, cd3x3, cproj], name='ch_concat_%s_chconcat' % name)
-  return concat
-
-
-prev = mx.symbol.Variable(name="Previos Output")
-in3a = InceptionFactoryA(prev, 64, 64, 64, 64, 96, "avg", 32, name="in3a")
-mx.viz.plot_network(symbol=in3a, shape=shape)
-vis.render('inception')
-
-# Load and Save
+# Load and save computation graph
 a = mx.sym.Variable('a')
-b = a * 2 + 1
-ex = b.bind(ctx=mx.cpu(), args={'a': mx.nd.ones((2, 3))})
-ex.forward()
-print(ex.outputs)
-print(len(ex.outputs))
-print(ex.outputs[0].asnumpy())
-print(b.tojson())
+b = mx.sym.Variable('b')
+c = a + b
 
 c.save('symbol-b.json')
 c2 = mx.symbol.load('symbol-b.json')
